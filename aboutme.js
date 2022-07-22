41fabd272857335c055da40c76c496f222a61e05
IshitaDeshar <ishita.deshar@gmail.com>
2022-07-22 12:03:25 +0545

    Initial Commit

---
+++ aboutme.js
@@ -0,0 +1,8 @@
+function showAboutMe() {
+  		var x = document.getElementById("aboutme");
+  		if (x.style.display === "none") {
+    	x.style.display = "block";
+  		} else {
+   		x.style.display = "none";
+  		}
+		}
